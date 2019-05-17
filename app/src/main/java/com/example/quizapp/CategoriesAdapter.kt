f2b4f10453684f0e372b7121d53f1a8cc9b7cf29
barchuckie <patryk_barczak@wp.pl>
2019-05-17 18:16:11 +0200

    Add CategoriesAdapter

---
+++ app/src/main/java/com/example/quizapp/CategoriesAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.quizapp
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.quizapp.DBModel.Category
+import kotlinx.android.synthetic.main.category_item_layout.view.*
+
+class CategoriesAdapter(private val data : List<Category>) :
+    RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder>() {
+
+    class CategoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.category_item_layout, parent, false)
+
+        return CategoriesViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
+        val categoryItem = data[position]
+        holder.itemView.categoryName.text = categoryItem.name
+    }
+
+}
