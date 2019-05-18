2676a31a0c47541d1bab958ae7d79b63f4355409
albi23 <piekielny7891>
2019-05-18 16:31:53 +0200

    Dochniu code merged (10%) in master

--- app/src/main/java/com/example/quizapp/CategoriesActivity.kt
+++ app/src/main/java/com/example/quizapp/CategoriesActivity.kt
@@ -1,11 +1,14 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.widget.Toast
 import com.example.quizapp.DBHelper.DBHelper
 import com.example.quizapp.DBModel.Category
+import com.example.quizapp.Game.GameActivity
 import kotlinx.android.synthetic.main.activity_categories.*
 import kotlinx.android.synthetic.main.content_categories.*
 
@@ -30,5 +33,9 @@ class CategoriesActivity : AppCompatActivity() {
     private fun playGame(category: Category) {
         /* Starts new game at proper category */
         Log.d("newGame", "new game start")
+        Toast.makeText(this,"${category.name}",Toast.LENGTH_LONG).show()
+        val intent = Intent(this,GameActivity::class.java)
+        startActivity(intent)
+
     }
 }
